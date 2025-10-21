33131cf4d931ce2165d78849b4a31a200697e79d
Yunus Mustafa Zanzibarwala <85278320+yunuszanzi@users.noreply.github.com>
2025-10-21 18:18:45 +0500

    code push

--- src/components/AppBarChart.tsx
+++ src/components/AppBarChart.tsx
@@ -1,14 +1,21 @@
 "use client";
-import { ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent, type ChartConfig } from "@/components/ui/chart";
+import {
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "@/components/ui/chart";
 import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 
 const chartConfig = {
-  desktop: {
-    label: "Desktop",
+  total: {
+    label: "Total",
     color: "var(--chart-1)",
   },
-  mobile: {
-    label: "Mobile",
+  successful: {
+    label: "Successful",
     color: "var(--chart-4)",
   },
 } satisfies ChartConfig;
@@ -36,11 +43,7 @@ const AppBarChart = () => {
             axisLine={false}
             tickFormatter={(value) => value.slice(0, 3)}
           />
-          <YAxis
-            tickLine={false}
-            tickMargin={10}
-            axisLine={false}
-          />
+          <YAxis tickLine={false} tickMargin={10} axisLine={false} />
           <ChartTooltip content={<ChartTooltipContent />} />
           <ChartLegend content={<ChartLegendContent />} />
           <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
